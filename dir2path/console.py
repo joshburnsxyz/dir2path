90c3937f4dabc137940d923264f1efbd48a5541c
Josh Burns <joshyburnss@gmail.com>
2022-12-29 16:58:57 +1100

    fix path errors

--- dir2path/console.py
+++ dir2path/console.py
@@ -10,8 +10,11 @@ def run():
     dirp = os.getcwd()
     shell_name = os.environ['SHELL'].split("/")[-1]
     if shell_name in supported_shells:
-        shell_config = supported_shells[shell_name]
+        shell_config = open(os.path.expanduser(supported_shells[shell_name]), 'a')
         path_append_script = f"export PATH=\"$PATH:{dirp}\"\n"
+        shell_config.write(path_append_script)
+        shell_config.close()
+        print(f"{dirp} appended to $PATH, please reload your shell")
         quit()
     else:
         print(f"Unsupported shell: {system_shell_var}")
