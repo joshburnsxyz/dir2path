e80f64294510848139e73b73d10df61e86e17528
Josh Burns <joshyburnss@gmail.com>
2022-12-29 16:48:10 +1100

    shell detection

--- dir2path/console.py
+++ dir2path/console.py
@@ -1,2 +1,19 @@
+import os
+
+
+supported_shells = {
+    "bash": "~/.bashrc",
+    "zsh": "~/.zshrc",
+}
+
 def run():
-    print("Hello world")
+    dirp = os.getcwd()
+    system_shell_var = os.environ['SHELL'].split("/")
+    shell_name = system_shell_var[-1]
+    print(shell_name)
+    if shell_name in supported_shells:
+        print(f"appending {dirp} to {shell_name}")
+        quit()
+    else:
+        print(f"Unsupported shell: {system_shell_var}")
+        quit(1)
